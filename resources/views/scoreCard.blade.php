d2cd27bb4b07b54f29f0aebf26423ff3a9f1cbd9
santosh <ssant0s@live.com>
2016-11-08 15:36:25 +1100

    latest modification

--- resources/views/scoreCard.blade.php
+++ resources/views/scoreCard.blade.php
@@ -2,9 +2,11 @@
 @section('content')
 <link rel ="stylesheet" href="{{'css/table.css'}}">
 <table class ="tbl">
+{!! Form::open(['url' => 'inserscore', 'files' => true]) !!}
 <tr class ="unique"> <td>Grade</td><td>{!! Form::text('grade') !!}</td></tr>
 <tr class ="unique"> <td>Feedback</td><td>{!!Form::textarea('feedback') !!}</td></tr>
 <tr class ="unique"><td>{!!Form::submit('submit') !!}</td></tr>
+{!!Form::close()!!!}
 
 </table>
 
